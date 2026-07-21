bd429e17ba6b980a6bb06aadf9972ce2b53ff85f
TJSarno <thomassarno@hotmail.com>
2026-07-21 14:14:38 +0100

    AES-508, scaffolded any discrepancies page

---
+++ test/forms/AnyDiscrepanciesFormProviderSpec.scala
@@ -0,0 +1,30 @@
+package forms
+
+import forms.behaviours.BooleanFieldBehaviours
+import forms.happyPath.AnyDiscrepanciesFormProvider
+import play.api.data.FormError
+
+class AnyDiscrepanciesFormProviderSpec extends BooleanFieldBehaviours {
+
+  val requiredKey = "anyDiscrepancies.error.required"
+  val invalidKey = "error.boolean"
+
+  val form = new AnyDiscrepanciesFormProvider()()
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like booleanField(
+      form,
+      fieldName,
+      invalidError = FormError(fieldName, invalidKey)
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+  }
+}
