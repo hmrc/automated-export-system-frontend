9f220a4434d859688d259dcece93634a3e22a7c7
Kat <gravesmorgan02@gmail.com>
2026-08-11 16:12:33 +0100

    AES 658, added controller, def and routes for cancelling submission.
    # Conflicts:
    #	app/uk/gov/hmrc/automatedexportsystemfrontend/controllers/submission/CancelSubmissionController.scala
    #	conf/app.routes

--- app/uk/gov/hmrc/automatedexportsystemfrontend/controllers/submission/CancelSubmissionController.scala
+++ app/uk/gov/hmrc/automatedexportsystemfrontend/controllers/submission/CancelSubmissionController.scala
@@ -1,32 +1,21 @@
-/*
- * Copyright 2026 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package uk.gov.hmrc.automatedexportsystem.controllers
 
-package uk.gov.hmrc.automatedexportsystemfrontend.controllers.submission
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import uk.gov.hmrc.automatedexportsystemfrontend.views.html.submission.CancelSubmissionView
-import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import javax.inject.{Inject, Singleton}
 
-import javax.inject.Inject
+@Singleton
+class CancelSubmissionController @Inject() (
+                                             cc: ControllerComponents
+                                           ) extends FrontendController(cc):
 
-class CancelSubmissionController @Inject() (override val controllerComponents: MessagesControllerComponents, view: CancelSubmissionView)
-    extends FrontendBaseController {
-
-  def onPageLoad(mrn: String): Action[AnyContent] =
+  def confirmCancellation(mrn: String): Action[AnyContent] =
     Action { implicit request =>
-      Ok(view(mrn))
+      Ok
     }
-}
+
+  def cancellationCompleted(mrn: String): Action[AnyContent] =
+    Action { implicit request =>
+      Ok
+    }
