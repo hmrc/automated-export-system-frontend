b7d55285e2bd56e843bc6cf6aaf21c67795d982f
Kevin Coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2026-08-17 11:56:41 +0100

    PIL-281: Updated imports following merge of main

--- app/uk/gov/hmrc/automatedexportsystemfrontend/navigation/UnhappyPathNavigator.scala
+++ app/uk/gov/hmrc/automatedexportsystemfrontend/navigation/UnhappyPathNavigator.scala
@@ -17,7 +17,7 @@
 package uk.gov.hmrc.automatedexportsystemfrontend.navigation
 
 import play.api.mvc.Call
-import uk.gov.hmrc.automatedexportsystemfrontend.controllers.happyPath.routes as happyRoute
+import uk.gov.hmrc.automatedexportsystemfrontend.controllers.create.routes as happyRoute
 import uk.gov.hmrc.automatedexportsystemfrontend.controllers.unhappyPath.routes as unhappyRoute
 import uk.gov.hmrc.automatedexportsystemfrontend.models.{NormalMode, UserAnswers}
 import uk.gov.hmrc.automatedexportsystemfrontend.navigation.Navigator
